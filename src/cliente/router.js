319fac725ce026cc1d2d17059b127212a3c747d7
Ricardo Pereira <ricardompb@outlook.com>
2025-10-07 20:40:57 -0300

    integração com banco de dados

--- src/cliente/router.js
+++ src/cliente/router.js
@@ -1,16 +1,22 @@
 import express from 'express';
-import utils from '../utils.js'
+import { Pool } from 'pg';
+
+const connection = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '123456',
+    database: 'aula-07-10-2025',
+});
 
 const router = express.Router();
 
-const clientes = [
-    { id: 1, nome: 'Ricardo', apelido: 'Ricardo' },
-    { id: 2, nome: 'Maria', apelido: 'Mary' },
-    { id: 3, nome: 'João', apelido: 'João' },
-];
+const clientes = [];
 
-function findById(id, res) {
-    const cliente = clientes.find(c => c.id === parseInt(id));
+async function findById(id, res) {
+    const sql = 'select * from cliente where id = $1';
+    const { rows } = await connection.query(sql, [id]);
+    const [cliente] = rows;
     if (!cliente) {
         return res.status(404).json({
             error: true,
@@ -21,38 +27,35 @@ function findById(id, res) {
 }
 
 // GET    http://localhost:3000/cliente - Listar todos os clientes
-router.get('/', (req, res) => {
-    res.json(clientes);
+router.get('/', async (req, res) => {
+    const sql = 'select * from cliente order by id';
+    const { rows } = await connection.query(sql);
+    return res.json(rows);
 })
 
 // GET    http://localhost:3000/cliente/:id - Obtem um cliente pelo seu identificador
-router.get('/:id', (req, res) => {
-    const cliente = findById(req.params.id, res);
+router.get('/:id', async (req, res) => {
+    const cliente = await findById(req.params.id, res);
     if (cliente) {
         return res.json(cliente);
     }    
 })
 
+const getNextId = async () => {
+    const { rows } = await connection.query('select max(id) as max from cliente');
+    const [row] = rows;
+    if (!row) return 1;
+    return row.max + 1;    
+}
+
 // POST   http://localhost:3000/cliente Cria um novo cliente
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const id = clientes.length + 1;
     try {
-        // clientes.push({ id, ...req.body });
-        const { nomeApelido, cpf, ...resto } = req.body;
-
-        if (!utils.checkCPF(cpf)) {
-            return res.status(400).json({
-                error: true,
-                message: 'CPF inválido'
-            })
-        }
-
-        clientes.push({ id, 
-            ...resto,
-            cpf,
-            apelido: nomeApelido
-         });
-
+        const id = await getNextId();
+        const { nome, apelido } = req.body;
+        await connection.query(sql);
+        await connection.query('insert into cliente (id, nome, apelido) values ($1, $2, $3)', [id, nome, apelido]);
         return res.status(201).json({
             message: 'Cliente cadastrado com sucesso'
         })
@@ -65,14 +68,11 @@ router.post('/', (req, res) => {
 })
 
 // PUT    http://localhost:3000/cliente/:id - Atualiza o cliente pelo seu identificador
-router.put('/:id', (req, res) => {
-    const cliente = findById(req.params.id, res);
-    if (cliente) {
-        cliente.nome = req.body.nome;
-        return res.json({
-            message: 'Cliente atualizado com sucesso'
-        })
-    }
+router.put('/:id', async (req, res) => {
+    await connection.query('update cliente set nome = $1 where id = $2', [req.body.nome, req.params.id])
+    res.json({
+        message: 'Cliente atualizado com sucesso'
+    })
 })
 
 // DELETE http://localhost:3000/cliente/:id - Remove o cliente pelo seu identificador
